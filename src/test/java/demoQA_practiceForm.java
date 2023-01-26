b433dcad94eb688171bb55cdcc6adad015b7a53a
Vadim Dinislamov <dvadim87@gmail.com>
2023-01-26 03:49:03 +0300

    rename class name

--- src/test/java/demoQA_practiceForm.java
+++ src/test/java/demoQA_practiceForm.java
@@ -1,17 +1,12 @@
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selectors;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.by;
-import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 import static  com.codeborne.selenide.Selenide.open;
 
-public class demoQApracticeForm {
+public class demoQA_practiceForm {
 
     @BeforeAll
     static void beforeAll(){
@@ -46,11 +41,14 @@ public class demoQApracticeForm {
         $x("//div[@id='userEmail-wrapper']//input[@id='userEmail']").setValue(userEmail);
         $x("//div[@id= 'genterWrapper']//label [text() = 'Male' and @for = 'gender-radio-1']").click();
         $x("//div[@id='userNumber-wrapper']//input[@id='userNumber']").setValue(userPhone);
+
+        //calendar picker
         $x("//input[@id='dateOfBirthInput']").click();
-//        $x("//select[@class='react-datepicker__month-select']").click();
         $x("//option[@value ='4']").click();
         $x("//option[@value ='1994']").click();
         $x("//div[@class ='react-datepicker__day react-datepicker__day--008 react-datepicker__day--weekend']").click();
+
+        // autocomplete form
         $x("//*[@id='subjectsContainer']").click();
         $x("//*[@class='subjects-auto-complete__input']").sendKeys("r");
 //        $x("//*[@id='subjectsContainer']").setValue("r");
